65a15f18c7165939eedd52acf736417efe6f1f4c
Harsh <iamhp2k@gmail.com>
2021-10-20 23:48:24 +0530

    Create Bubble.java

---
+++ hp/Bubble.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bubble {
+    public static void BubbleSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("\n");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        BubbleSort(a);
+    }
+}
